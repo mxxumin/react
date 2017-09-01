e0fc30e45edc967cb0e414107990dc03d3434369
xumin2by <mxxumin@163.com>
2017-09-01 18:42:29 +0800

    commentApp-20170901

---
+++ react-component/comment-app/src/CommentInput.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class CommentInput extends Component{
+	render(){
+		return(
+			<div>
+			    CommentInput
+			</div>
+		)
+	}
+}
+export default CommentInput;
